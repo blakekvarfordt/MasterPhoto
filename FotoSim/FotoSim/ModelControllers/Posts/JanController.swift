2a2d525f9eb6d594cc6277881695653c49823874
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-09-29 22:26:29 -0600

    sets up createJanuaryPost function in JanController

--- FotoSim/FotoSim/ModelControllers/Posts/JanController.swift
+++ FotoSim/FotoSim/ModelControllers/Posts/JanController.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2019 Blake kvarfordt. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CloudKit
+
+class JanController {
+    
+    static let shared = JanController()
+    
+    var posts = [JanuaryPost]()
+    
+    let publicDatabase = CKContainer.default().publicCloudDatabase
+    
+    
+    func createPost(title: String, description: String, image: UIImage, userProfileImage: UIImage, userThatPosted: String, userReference: CKRecord.Reference, completion: @escaping (Bool) -> Void) {
+        
+        let post = JanuaryPost(title: title, description: description, userReference: userReference, image: image, userThatPosted: userThatPosted, userProfileImage: userProfileImage)
+        if post.isBlocked == [] {
+            post.isBlocked.append("Default")
+        }
+        let postRecord = CKRecord(januaryPost: post)
+        publicDatabase.save(postRecord) { (record, error) in
+            
+            if let error = error {
+                print("Error saving a record to database in \(#function) \(error) \(error.localizedDescription)")
+                completion(false)
+                return
+            }
+            
+            guard let record = record, let post = JanuaryPost(ckRecord: record) else { completion(false); return }
+            self.posts.append(post)
+            completion(true)
+        }
+    }
+}
