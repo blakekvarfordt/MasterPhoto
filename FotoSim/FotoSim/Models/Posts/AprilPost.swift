5343728617612b634c17080b1a5a82061eeaecc8
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-09-30 07:49:26 -0600

    sets up AprilPost model

--- FotoSim/FotoSim/Models/Posts/AprilPost.swift
+++ FotoSim/FotoSim/Models/Posts/AprilPost.swift
@@ -6,4 +6,137 @@
 //  Copyright © 2019 Blake kvarfordt. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CloudKit
+
+struct AprilPostConstants {
+    static let recordKey = "AprilPost"
+    static let titleKey = "Title"
+    static let descriptionKey = "Description"
+    static let timestampKey = "Timestamp"
+    static let imageKey = "Image"
+    static let userReferenceKey = "UserReference"
+    static let userThatPostedKey = "UserThatPosted"
+    static let userProfileImageKey = "UserProfileImage"
+    static let isBlockedKey = "isBlocked"
+}
+
+class AprilPost {
+    let title: String
+    let description: String
+    let timestamp: Date
+    let userThatPosted: String?
+    let recordID: CKRecord.ID
+    let userReference: CKRecord.Reference?
+    var isBlocked: [String] = []
+    var imageData: Data?
+    var image: UIImage? {
+        get {
+            guard let imageData = imageData else { return nil }
+            return UIImage(data: imageData)
+        } set {
+            imageData = newValue?.jpegData(compressionQuality: 0.5)
+        }
+    }
+    
+    var imageAsset: CKAsset? {
+        let tempDict = NSTemporaryDirectory()
+        let tempDictURL = URL(fileURLWithPath: tempDict)
+        let fileURL = tempDictURL.appendingPathComponent(UUID().uuidString).appendingPathExtension("jpg")
+        do {
+            try imageData?.write(to: fileURL)
+        } catch {
+            print("Error writing to temp URL \(error) \(error.localizedDescription)")
+        }
+        return CKAsset(fileURL: fileURL)
+    }
+    
+    var userProfileImageData: Data?
+    var userProfileImage: UIImage? {
+        get {
+            guard let imageData = userProfileImageData else { return nil }
+            return UIImage(data: imageData)
+        } set {
+            userProfileImageData = newValue?.jpegData(compressionQuality: 0.5)
+        }
+    }
+    
+    var userProfileImageAsset: CKAsset? {
+        let tempDictionary = NSTemporaryDirectory()
+        let tempDictionaryURL = URL(fileURLWithPath: tempDictionary)
+        let userProfileImageFileURL = tempDictionaryURL.appendingPathComponent(UUID().uuidString).appendingPathExtension("jpgs")
+        do {
+            try userProfileImageData?.write(to: userProfileImageFileURL)
+        } catch {
+            print("Error writing to temp URL \(error) \(error.localizedDescription)")
+        }
+        return CKAsset(fileURL: userProfileImageFileURL)
+    }
+    
+    init(title: String, description: String, timestamp: Date = Date(), recordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString), userReference: CKRecord.Reference, image: UIImage, userThatPosted: String, userProfileImage: UIImage) {
+        
+        self.title = title
+        self.description = description
+        self.timestamp = timestamp
+        self.recordID = recordID
+        self.userReference = userReference
+        self.userThatPosted = userThatPosted
+        self.userProfileImage = userProfileImage
+        self.image = image
+    }
+    
+    init?(ckRecord: CKRecord) {
+        guard let title = ckRecord[AprilPostConstants.titleKey] as? String,
+            let description = ckRecord[AprilPostConstants.descriptionKey] as? String,
+            let timestamp = ckRecord[AprilPostConstants.timestampKey] as? Date,
+            let userReference = ckRecord[AprilPostConstants.userReferenceKey] as? CKRecord.Reference,
+            let imageAsset = ckRecord[AprilPostConstants.imageKey] as? CKAsset,
+            let userThatPosted = ckRecord[AprilPostConstants.userThatPostedKey] as? String,
+            let userProfileImageAsset = ckRecord[AprilPostConstants.userProfileImageKey] as? CKAsset else { return nil }
+        
+        self.title = title
+        self.description = description
+        self.timestamp = timestamp
+        self.userReference = userReference
+        self.userThatPosted = userThatPosted
+        self.recordID = ckRecord.recordID
+        if let isBlocked = ckRecord[AprilPostConstants.isBlockedKey] as? [String] {
+            self.isBlocked = isBlocked
+        }
+        
+        
+        guard let url = imageAsset.fileURL else { return }
+        
+        do {
+            self.imageData = try Data(contentsOf: url)
+        } catch {
+            print("Error converting imageAsset to Data \(error) \(error.localizedDescription)")
+        }
+        
+        guard let profileImageURL = userProfileImageAsset.fileURL else { return }
+        
+        do {
+            self.userProfileImageData = try Data(contentsOf: profileImageURL)
+        } catch {
+            print("Error converting imageAsset to Data \(error) \(error.localizedDescription)")
+        }
+        
+    }
+}
+
+extension CKRecord {
+    convenience init(aprilPost: AprilPost) {
+        self.init(recordType: AprilPostConstants.recordKey, recordID: aprilPost.recordID)
+        self.setValue(aprilPost.title, forKey: AprilPostConstants.titleKey)
+        self.setValue(aprilPost.description, forKey: AprilPostConstants.descriptionKey)
+        self.setValue(aprilPost.timestamp, forKey: AprilPostConstants.timestampKey)
+        self.setValue(aprilPost.imageAsset, forKey: AprilPostConstants.imageKey)
+        self.setValue(aprilPost.userProfileImageAsset, forKey: AprilPostConstants.userProfileImageKey)
+        self.setValue(aprilPost.userReference, forKey: AprilPostConstants.userReferenceKey)
+        self.setValue(aprilPost.userThatPosted, forKey: AprilPostConstants.userThatPostedKey)
+        
+        if !aprilPost.isBlocked.isEmpty {
+            self.setValue(aprilPost.isBlocked, forKey: AprilPostConstants.isBlockedKey)
+        }
+    }
+}
